b0a12ec95d1856b619148340c82d882bd23790ba
AOx0 <50227494+AOx0@users.noreply.github.com>
2020-07-23 20:21:11 -0500

    Finished Even Fibonacci Numbers

--- EvenFibonacciNumbers/main.swift
+++ EvenFibonacciNumbers/main.swift
@@ -1,3 +1,12 @@
-numero = 1
-suma = 2
-
+var firstNumber : Int = 1
+var secondNumber : Int = 1
+var newNumber : Int = 0
+var sum = 0
+while true{
+    guard (firstNumber + secondNumber) < 4000000 else { break }
+    newNumber = firstNumber + secondNumber
+    if newNumber % 2 == 0 {sum += newNumber}
+    firstNumber = secondNumber
+    secondNumber = newNumber
+}
+print(sum)
--- MultiplesOf3And5/main.swift
+++ MultiplesOf3And5/main.swift
@@ -1,8 +1,5 @@
-var sumaTotal = 0
+var sum = 0
 for i in 3..<1000 {
-    if i.isMultiple(of: 5) || i.isMultiple(of: 3) {
-        sumaTotal += i
-    }
+    if i.isMultiple(of: 5) || i.isMultiple(of: 3) { sum += i }
 }
-
-print(sumaTotal)
+print(sum)
